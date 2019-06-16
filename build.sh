25dcc3d65b11542f343e72cf0ad664738ad56bb1
Ali Berenjian <ali.rberenjian@gmail.com>
2019-06-15 20:30:58 -0700

    Split and add Bash Site Generator

---
+++ build.sh
@@ -0,0 +1,5 @@
+cat ./template/top_clear_navbar.html ./content/index.html ./template/bottom.html > ./index.html
+cat ./template/top_white_navbar.html ./content/about.html ./template/bottom.html > ./about.html
+cat ./template/top_white_navbar.html ./content/projects.html ./template/bottom.html > ./projects.html
+
+
