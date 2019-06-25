e4faea3320e282900eac174d6a507e0bd1e42a30
Alireza Berenjian <Ali.rberenjian@gmail.com>
2019-06-25 13:07:16 -0700

    Complete HW3

--- build.py
+++ build.py
@@ -1,21 +1,49 @@
-top_clear_navbar= open('./template/top_clear_navbar.html').read()
-top_white_navbar= open('./template/top_white_navbar.html').read()
-bottom = open('./template/bottom.html').read()
+def main():
+  pages = [
+    { 
+      "filename": './content/index.html',
+      "output": 'docs/index.html',
+      "title": "Home"
+    },
+    {
+      "filename": './content/about.html',
+      "output": 'docs/about.html',
+      "title": "About"
+    },
+    {
+      "filename": './content/projects.html',
+      "output": 'docs/projects.html',
+      "title": "Projects"
+    }
+  ]
+  for page in pages:
+    if page["title"] == 'Home':
+      template = open('./template/base_clear_navbar.html').read()
+    else:
+      template = open('./template/base_white_navbar.html').read()
+    content = open(page["filename"]).read()
+    final_content = replace_content(template,content)
+    final_content = replace_title(final_content,page["title"])
+    final_content = replace_active_link(final_content,page["title"])
+    apply_template(page["output"],final_content)
 
 
-# Build Index Page
-index = open('./content/index.html').read()
-new_index = top_clear_navbar + index + bottom
-open('./docs/index.html','w+').write(new_index)
+def replace_active_link(final_content,title):
+  if title == 'Home':
+    return final_content.replace("{{active_home}}",'class="active"')
+  elif title == 'About':
+    return final_content.replace("{{active_about}}",'class="active"')
+  else:
+    return final_content.replace("{{active_projects}}",'class="active"')
 
-# Build About Page
-about = open('./content/about.html').read()
-new_about = top_white_navbar + about + bottom
-open('./docs/about.html','w+').write(new_about)
+def replace_content(template,content):
+  return template.replace("{{content}}",content)
 
-# Build Projects Page
-projects = open('./content/projects.html').read()
-new_projects = top_white_navbar + projects + bottom
-open('./docs/projects.html','w+').write(new_projects)
+def apply_template(outputpath,final_content):
+    open(outputpath, 'w+').write(final_content)
 
+def replace_title(final_content,title):
+  return final_content.replace("{{title}}",title)
 
+if __name__ == "__main__":
+  main()
